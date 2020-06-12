29c873fc04d07c1100bcbe8effa990f84459cf27
Alimjan921 <attila0921@hotmail.com>
2020-06-12 18:43:23 +0200

    update HelloGitWorld.java

--- src/tests/HelloGitWorld.java
+++ src/tests/HelloGitWorld.java
@@ -5,11 +5,7 @@ public class HelloGitWorld {
     public static void main(String[] args) {
         System.out.println("Hello new world again!!");
 
-
-
-
-
-
+        System.out.println("some code");
 
     }
 }
