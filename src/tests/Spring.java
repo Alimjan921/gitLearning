c49c5f8243d3c6644b874cada3c4c2885f30dfd1
Alimjan921 <attila0921@hotmail.com>
2020-06-12 21:23:07 +0200

    add Task.java

--- src/tests/Spring.java
+++ src/tests/Spring.java
@@ -4,6 +4,7 @@ public class Spring {
     public static void main(String[] args) {
         System.out.println("Good bye Spring...");
 
+        System.out.println("Hello summer");
 
     }
 }
---
+++ src/tests/Task.java
@@ -0,0 +1,7 @@
+package tests;
+
+public class Task {
+    public static void main(String[] args) {
+        System.out.println("some code");
+    }
+}
